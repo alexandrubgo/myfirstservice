da33106702246ca421f9f6a86cb9238f41bada0f
Gelu <bornonparadise@gmail.com>
2021-10-08 00:39:03 +0300

    add main

---
+++ main.go
@@ -0,0 +1,40 @@
+// main.go
+package main
+
+import (
+	"database/sql"
+	"log"
+	geluHttp "mysql/delivery/http"
+	"mysql/repository"
+	"mysql/services"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:123qweasdzxc@tcp(127.0.0.1:3306)/database")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	userMysqlRepo := repository.NewUserMysqlRepository(db)
+	userServices := services.NewUserService(userMysqlRepo)
+	userHttp := geluHttp.NewUserHttp(userServices)
+
+	r := mux.NewRouter()
+
+	// GET
+	r.HandleFunc("/players", userHttp.GetPlayers)
+	// GET
+	r.HandleFunc("/players/{id}", userHttp.GetPlayer)
+	// POST
+	r.HandleFunc("/playersx", userHttp.CreatePlayer).Methods("POST")
+	//DELETE
+	r.HandleFunc("/players/remove/{id}", userHttp.DeletePlayer)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
